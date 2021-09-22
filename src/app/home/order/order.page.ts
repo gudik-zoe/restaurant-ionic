10fd0afa4ed6a968614ca2d18fef509e7836d201
vcastillo <verhydson.castillo@scaiconsulting.it>
2021-09-22 22:59:47 +0200

    global styling + actionSheet to be completed

--- src/app/home/order/order.page.ts
+++ src/app/home/order/order.page.ts
@@ -2,7 +2,7 @@
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/member-ordering */
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ActionSheetController, ModalController } from '@ionic/angular';
 import { Order } from 'src/app/models/order';
 import { ErrorHandlerService } from 'src/app/utility/error-handler.service';
 import { OrderModalComponent } from './order-modal/order-modal.component';
@@ -17,7 +17,8 @@ export class OrderPage implements OnInit {
   constructor(
     private orderService: OrderService,
     private errorHandler: ErrorHandlerService,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private actionSheetCtrl: ActionSheetController
   ) {}
   myOrders: Order[];
   order: Order;
@@ -52,6 +53,31 @@ export class OrderPage implements OnInit {
     } catch (err) {
       this.errorHandler.showError(err);
     }
+  }
+  getOrderDetails(order: Order) {
+    this.actionSheetCtrl
+      .create({
+        header: 'order details',
+        buttons: [
+          {
+            text: 'Select date',
+            handler: () => {
+              console.log('select date');
+            },
+          },
+          {
+            text: 'Random date',
+            handler: () => {
+              console.log('random  date');
+            },
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel',
+          },
+        ],
+      })
+      .then((actionSheetEl) => actionSheetEl.present());
   }
 
   ngOnInit() {
